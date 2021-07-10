20d2a0b02af8a6fdafe6f112fe76b17629dde7c9
monntauk <68526374+monntauk@users.noreply.github.com>
2021-07-09 21:35:06 -0400

    Delete Entrega3/MiProyecto/core directory

--- Entrega3/MiProyecto/core/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from django.forms import ModelForm
-from .models import Noticia
-
-class NoticiaForm(ModelForm):
-    class Meta:
-        model = Noticia
-        fields = ['idNoticia','resumen','titulo','categoria']
